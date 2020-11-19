db4dfef6e19e500e930b714faa08518cf22cf2ab
mohamed <mohamedhassansulatan@gmail.com>
2020-11-19 22:56:45 +0300

    widget component

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,46 @@ void main()=>runApp(CalculatorApp());
     class _CalcultatorState extends State<Calcultator> {
       @override
       Widget build(BuildContext context) {
-        return Container();
+        return Scaffold(
+          backgroundColor: Colors.white,
+          body: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 5),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: <Widget>[
+              Row(
+               mainAxisAlignment: MainAxisAlignment.end,
+                children: <Widget>[
+                  Padding(
+                    padding: EdgeInsets.all(10),
+                    child: Text(
+                      '0',
+                      textAlign: TextAlign.left,
+                      style: TextStyle(color: Colors.black,fontSize: 20),
+                    ),
+
+                  )
+                ],
+              )
+              ],
+            ),
+          )
+        );
       }
+    }
+
+    Widget calculatorButton(String btnText, Color btnColor, Color btnTextColor,onpress){
+      return Container(
+        child: RaisedButton(
+          onPressed: onpress,
+          child: Text(
+            btnText,
+            style: TextStyle(fontSize: 30,color: btnTextColor),
+          ),
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+          color: btnColor,
+          padding: EdgeInsets.all(20),
+        ),
+      );
     }
     
