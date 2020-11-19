0769a008eba4c71d2341f4e5c50b5449188e18a0
mohamed <mohamedhassansulatan@gmail.com>
2020-11-19 21:53:33 +0300

    flutter calcultator

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+void main()=>runApp(calculatorApp())
+
+    class calculatorApp  extends StatelessWidget {
+      @override
+      Widget build(BuildContext context) {
+        return Container();
+      }
+    }
