abb43de006f9264389e81a7ba261ef4cbb77e99c
mohamed <mohamedhassansulatan@gmail.com>
2020-11-19 22:05:46 +0300

    statefull-stateless widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,26 @@
 import 'package:flutter/material.dart';
 
-void main()=>runApp(calculatorApp())
+void main()=>runApp(CalculatorApp());
 
-    class calculatorApp  extends StatelessWidget {
+    class CalculatorApp  extends StatelessWidget {
+      @override
+      Widget build(BuildContext context) {
+        return MaterialApp(
+          home:Calcultator()
+        );
+      }
+    }
+    
+    class Calcultator extends StatefulWidget {
+      @override
+      _CalcultatorState createState() => _CalcultatorState();
+
+    }
+    
+    class _CalcultatorState extends State<Calcultator> {
       @override
       Widget build(BuildContext context) {
         return Container();
       }
     }
+    
